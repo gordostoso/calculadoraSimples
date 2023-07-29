e91cb80c99a748196d94e3a2b9c48a10beaa1384
gordostoso <gordostoso2@gmail.com>
2023-07-29 14:57:16 -0300

    Fist calculator

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 
+operetor = input("enter one operetor: + - * / ")
+num_1 = float(input("enter the 1st number"))
+num_2 = float(input("enter the 2nd number"))
+
+if operetor == "+":
+    print(num_1+ num_2)
+elif operetor == "-":
+    print(num_1 - num_2)
+elif operetor == "*":
+    print(num_1 * num_2)
+elif operetor == "/":
+    print(num_1 / num_2)
+else:
+    print("invalid operetor")
